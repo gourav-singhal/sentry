9f0a6ad9ca33722b2f889219840dcfc8d2edc42d
Danny Lee <dlee@sentry.io>
2020-09-10 21:11:12 -0700

    feat(perf): Update components to use CommitterStore

--- src/sentry/static/sentry/app/components/events/eventEntries.jsx
+++ src/sentry/static/sentry/app/components/events/eventEntries.jsx
@@ -207,9 +207,10 @@ class EventEntries extends React.Component {
             <EventCauseEmpty organization={organization} project={project} />
           ) : (
             <EventCause
+              organization={organization}
+              project={project}
               event={event}
-              orgId={organization.slug}
-              projectId={project.slug}
+              group={group}
             />
           ))}
         {event.userReport && group && (
